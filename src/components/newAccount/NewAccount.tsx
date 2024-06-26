057f1164770c04a05ca5074c1bd916adc19b97a1
Pedro Ramirez <prlara2000@gmail.com>
2024-06-25 22:41:55 -0500

    add state on create account

--- src/components/newAccount/NewAccount.tsx
+++ src/components/newAccount/NewAccount.tsx
@@ -1,14 +1,54 @@
+import { useState } from 'react';
+// import { Link } from 'react-router-dom';
 import './NewAccount.css';
 function NewAccount() {
+  const [newUser, setNewUser] = useState({ name: '', lastName: '', email: '', password: '' });
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const handleNewUser = (e) => {
+    e.preventDefault();
+    console.log(newUser);
+  };
   return (
     <div id='new-account'>
       <div className='card'>
         <p className='card-title'>Create a new account</p>
-        <form className='form'>
-          <input className='username' type='text' placeholder='First name' />
-          <input className='username' type='text' placeholder='Last name' />
-          <input className='username' type='text' placeholder='Email' />
-          <input className='password' type='password' placeholder='Password' />
+        <form className='form' onSubmit={handleNewUser}>
+          <input
+            className='new-user-input'
+            type='text'
+            placeholder='First name'
+            value={newUser.name}
+            onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+          />
+          <input
+            className='new-user-input'
+            type='text'
+            placeholder='Last name'
+            value={newUser.lastName}
+            onChange={(e) => setNewUser({ ...newUser, lastName: e.target.value })}
+          />
+          <input
+            className='new-user-input'
+            type='text'
+            placeholder='Email'
+            value={newUser.email}
+            onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+          />
+          <input
+            className='new-user-input'
+            type='password'
+            placeholder='Password'
+            value={newUser.password}
+            onChange={(e) => setNewUser({ ...newUser, password: e.target.value })}
+          />
+          <input
+            className='new-user-input'
+            type='password'
+            placeholder='Re enter-Password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+
           <button className='create-account-button'>Create Account</button>
         </form>
       </div>
