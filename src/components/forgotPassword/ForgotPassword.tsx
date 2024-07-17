ec967da1f4d5d10248048669dc54fd95b55e817b
Pedro Ramirez <prlara2000@gmail.com>
2024-07-16 22:45:32 -0500

    add forgot password

--- src/components/forgotPassword/ForgotPassword.tsx
+++ src/components/forgotPassword/ForgotPassword.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function ForgotPassword() {
-  return <div>this is the forgot password page</div>;
+  return (
+    <div className='right-section'>
+      <div className='card'>
+        <p className='card-title'>Reset your password</p>
+        <form className='form'>
+          <input className='username' type='text' placeholder='Email or phone number' />
+          <Link to='/'>
+            <button className='login-button'>Send reset link</button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPassword;
